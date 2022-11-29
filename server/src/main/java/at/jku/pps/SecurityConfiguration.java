0531af88ad85ae957f183003f9e9e5c97dfb9948
LukasFeldler <l.feldler1999@gmail.com>
2022-11-29 17:40:33 +0100

    updated Version 3

--- server/src/main/java/at/jku/pps/SecurityConfiguration.java
+++ server/src/main/java/at/jku/pps/SecurityConfiguration.java
@@ -22,13 +22,13 @@ public class SecurityConfiguration {
 
         UserDetails worker = User.builder()
                 .username("worker")
-                .password("{bcrypt}" + "$2a$16$WTV6XOp59UhELSmHmmbUwuQ68ziOt4IFFtmRZg6R8OdLlsN327kL2")
+                .password("{bcrypt}" + "$2a$16$c6HKq6qdo00ljQGgYDI5BOzGVRn1R3UpEFy7Bav/eqLk/nUfNMu7q")
                 .roles("WORKER")
                 .build();
 
         UserDetails manager = User.builder()
                 .username("manager")
-                .password("{bcrypt}" + "$2a$16$/8X0enzAOPB8fx3FZDcOy.BA/cIs3LqzCNGOV9tntDS35OALmwwzW")
+                .password("{bcrypt}" + "$2a$16$jlSfPHQCMYsC40mfeibi9eK0N7QTHivVbLBy4a66qyYJaPCImW9j6")
                 .roles("MANAGER")
                 .build();
 
@@ -38,15 +38,15 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> auth
-                .mvcMatchers(HttpMethod.GET,"/orders").authenticated()
-                .mvcMatchers(HttpMethod.GET,"/sortedOrders").authenticated()
-                .mvcMatchers(HttpMethod.POST,"/newOrderFirst").hasRole("ProductionManager")
-                .mvcMatchers(HttpMethod.POST,"/newOrderLast").hasRole("ProductionManager")
-                .mvcMatchers(HttpMethod.POST,"/newOrderAt").hasRole("ProductionManager")
-                .mvcMatchers(HttpMethod.PUT,"/changeOrderDescription").authenticated()
-                .mvcMatchers(HttpMethod.PUT,"/changeOrderPriority").authenticated()
-                .mvcMatchers(HttpMethod.DELETE,"/deleteOrders").hasRole("ProductionManager")
-                .mvcMatchers(HttpMethod.DELETE,"/deleteAllOrder").hasRole("ProductionManager")
+                .mvcMatchers(HttpMethod.GET,"/orders").permitAll()
+                .mvcMatchers(HttpMethod.GET,"/sortedOrders").permitAll()
+                .mvcMatchers(HttpMethod.POST,"/newOrderFirst").hasRole("MANAGER")
+                .mvcMatchers(HttpMethod.POST,"/newOrderLast").hasRole("MANAGER")
+                .mvcMatchers(HttpMethod.POST,"/newOrderAt").hasRole("MANAGER")
+                .mvcMatchers(HttpMethod.PUT,"/changeOrderDescription").permitAll()
+                .mvcMatchers(HttpMethod.PUT,"/changeOrderPriority").permitAll()
+                .mvcMatchers(HttpMethod.DELETE,"/deleteOrders").hasRole("MANAGER")
+                .mvcMatchers(HttpMethod.DELETE,"/deleteAllOrder").hasRole("MANAGER")
         ).httpBasic(Customizer.withDefaults());
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.csrf().disable();
